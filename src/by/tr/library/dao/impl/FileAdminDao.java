096f28847a9498c159a56884542c52d033c87102
larisabigvava <bigvavalarisa@mail.ru>
2016-07-11 08:36:47 +0300

    implementation of adding book

--- src/by/tr/library/dao/impl/FileAdminDao.java
+++ src/by/tr/library/dao/impl/FileAdminDao.java
@@ -1,13 +1,29 @@
 package by.tr.library.dao.impl;
 
 import by.tr.library.bean.Book;
+import by.tr.library.bean.ProgrammerBook;
 import by.tr.library.dao.AdminDao;
 import by.tr.library.dao.exception.DAOException;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by Larisa_Bigvava on 7/8/2016.
  */
 public class FileAdminDao implements AdminDao {
+
+    private final static FileAdminDao instance = new FileAdminDao();
+
+    private final static String fileName = System.getProperty("java.class.path") + "/books.txt";
+
+    private FileAdminDao() {
+    }
+
+    public static FileAdminDao getInstance() {
+        return instance;
+    }
 
     @Override
     public boolean blockUserById(int idUser) throws DAOException {
@@ -17,8 +33,14 @@ public class FileAdminDao implements AdminDao {
 
     @Override
     public boolean addNewBook(Book book) throws DAOException {
-        //TODO
-        return false;
+        try (FileOutputStream fos = new FileOutputStream(fileName, true)) {
+            PrintWriter writer = new PrintWriter(fos);
+            writer.println(prepare(book));
+            writer.flush();
+        } catch (IOException ex) {
+            throw new DAOException(ex.getMessage(), ex);
+        }
+        return true;
     }
 
     @Override
@@ -37,5 +59,21 @@ public class FileAdminDao implements AdminDao {
     public boolean deleteUserByLogin(String login) throws DAOException {
         //TODO
         return false;
+    }
+
+    private String prepare(Book book) {
+        StringBuilder record = new StringBuilder(book.getTitle());
+            record.append("::")
+                    .append(book.getAuthor())
+                    .append("::")
+                    .append(book.getPrice());
+        if (book.getClass() == ProgrammerBook.class) {
+            ProgrammerBook programmerBook = (ProgrammerBook) book;
+            record.append("::")
+                    .append(programmerBook.getLevel())
+                    .append("::")
+                    .append(programmerBook.getLanguage());
+        }
+        return record.toString();
     }
 }
