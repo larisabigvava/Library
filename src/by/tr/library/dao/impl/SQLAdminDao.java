9f0da2492037c8f6d1c15d5e8fe4981795f6b82a
Larisa_Bigvava <bigvavalarisa@mail.ru>
2016-07-15 15:49:24 +0300

    fileds added

--- src/by/tr/library/dao/impl/SQLAdminDao.java
+++ src/by/tr/library/dao/impl/SQLAdminDao.java
@@ -5,6 +5,24 @@ import by.tr.library.dao.AdminDao;
 import by.tr.library.dao.exception.DAOException;
 
 public class SQLAdminDao implements AdminDao {
+	private static final String COLUMN_NAME_LOGIN = "login";
+	private static final String COLUMN_NAME_PASSWORD = "password";
+
+	private static final String COLUMN_NAME_TITLE = "title";
+	private static final String COLUMN_NAME_AUTHOR = "author";
+	private static final String COLUMN_NAME_PRICE = "price";
+	private static final String COLUMN_NAME_LEVEL = "level";
+	private static final String COLUMN_NAME_LANGUAGE = "language";
+
+	private static final String UPDATE_USER_BLOCKED = "UPDATE `users` SET `blocked`=? WHERE `login`=?";
+	private static final String UPDATE_USER_UNBLOCKED = "UPDATE `users` SET `blocked`=? WHERE `login`=?";
+	private static final String DELETE_USER_BY_LOGIN = "DELETE FROM `users` WHERE `login`=?";
+
+	private static final String INSERT_BOOK = "INSERT INTO `books`(`title`,`author`,`price`) VALUES(?,?,?)";
+	private static final String INSERT_PROGRAMMER_BOOK = "INSERT INTO books(`title`,`author`,`price`," +
+			"`language`,`level`) VALUES(?,?,?,?,?)";
+	private static final String DELETE_BOOK_BY_TITLE = "DELETE FROM `books` WHERE `title`=?";
+	private static final String DELETE_PROGRAMMER_BOOK_BY_TITLE = "DELETE FROM `programmer_books` WHERE `title`=?";
 
 	@Override
 	public boolean blockUserByLogin(String login) throws DAOException {
